a9ae4bb6dce15d2dfac3fa7a9e8fe185527ec4db
Andrei Lebedev <lebdron@gmail.com>
2018-06-21 13:08:19 +0300

    Fix parallelism parameter on Windows
    Signed-off-by: Andrei Lebedev <lebdron@gmail.com>

--- .jenkinsci/bindings.groovy
+++ .jenkinsci/bindings.groovy
@@ -22,7 +22,8 @@ def doJavaBindings(os, buildType=Release) {
       -DSWIG_JAVA=ON \
       ${cmakeOptions}
   """
-  sh "cmake --build build --target irohajava -- -j${params.PARALLELISM}"
+  def parallelismParam = (os == 'windows') ? '/MP' : '-j'
+  sh "cmake --build build --target irohajava -- ${parallelismParam}${params.PARALLELISM}"
   // TODO 29.05.18 @bakhtin Java tests never finishes on Windows Server 2016. IR-1380
   sh "zip -j $artifactsPath build/bindings/*.java build/bindings/*.dll build/bindings/libirohajava.so"
   if (os == 'windows') {
@@ -59,7 +60,8 @@ def doPythonBindings(os, buildType=Release) {
       -DSUPPORT_PYTHON2=$supportPython2 \
       ${cmakeOptions}
   """
-  sh "cmake --build build --target irohapy -- -j${params.PARALLELISM}"
+  def parallelismParam = (os == 'windows') ? '/MP' : '-j'
+  sh "cmake --build build --target irohapy -- ${parallelismParam}${params.PARALLELISM}"
   sh "cmake --build build --target python_tests"
   sh "cd build; ctest -R python --output-on-failure"
   if (os == 'linux') {
